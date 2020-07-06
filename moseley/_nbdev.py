8de5b09fceb0bfc244d8e5ac72600b980ae2362a
Frank Ligterink <frank.ligterink@lombok.nl>
2020-07-06 10:56:00 +0200

    moved explanation to mplot

--- moseley/_nbdev.py
+++ moseley/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"abc": "00_mplot.ipynb"}
 
-modules = []
+modules = ["mplot.py"]
 
 doc_url = "https://fligt.github.io/moseley/"
 
