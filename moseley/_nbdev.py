cb4d267bcc3782b15daaebee6f2cd4ab9e85fe1d
Frank Ligterink <frank.ligterink@lombok.nl>
2020-07-10 23:56:20 +0200

    moseley plot code

--- moseley/_nbdev.py
+++ moseley/_nbdev.py
@@ -2,7 +2,14 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"abc": "00_mplot.ipynb"}
+index = {"make_ptable": "00_mplot.ipynb",
+         "colorize": "00_mplot.ipynb",
+         "draw_box": "00_mplot.ipynb",
+         "ptable_plot": "00_mplot.ipynb",
+         "XFluo": "00_mplot.ipynb",
+         "moseley_law": "00_mplot.ipynb",
+         "moseley_plot": "00_mplot.ipynb",
+         "elementsInstance": "00_mplot.ipynb"}
 
 modules = ["mplot.py"]
 
